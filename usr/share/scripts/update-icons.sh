97eaea93741b66f3149e0dd9a45d04650e2fcbaa
Jan Biernacik <jan@biernacik.org>
2023-12-15 22:05:12 +0100

    init

--- usr/share/scripts/update-icons.sh
+++ usr/share/scripts/update-icons.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 
+mods_dir="/usr/share/mods/"
+applications_dir="/usr/share/applications/"
+
 # Check if source and destination directories exist
-if [ -d "/usr/share/mods/" ] && [ -d "/usr/share/applications/" ]; then
-    # Copy specific files or handle the copying process as required
-    cp /usr/share/mods/* /usr/share/applications/
+if [ -d "$mods_dir" ] && [ -d "$applications_dir" ]; then
+    # Loop through files in mods directory
+    for file in "$mods_dir"*; do
+        # Extract the filename without the path
+        filename=$(basename "$file")
+        # Check if the corresponding file exists in applications directory
+        if [ -f "$applications_dir$filename" ]; then
+            cp "$mods_dir$filename" "$applications_dir"
+            echo "$filename updated successfully."
+        else
+            echo "File $filename does not exist in the applications directory. Skipping..."
+        fi
+    done
     echo "Icons updated successfully."
 else
     echo "Source or destination directory does not exist."
 fi
+
